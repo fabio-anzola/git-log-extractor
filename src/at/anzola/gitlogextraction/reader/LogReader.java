58626bb7913ba4f404c565a4e5fa785ef93ad319
Anzola Fabio <anzola.fabio@gmail.com>
2020-12-04 18:20:14 +0100

    LogReader now using Log file

--- src/at/anzola/gitlogextraction/reader/LogReader.java
+++ src/at/anzola/gitlogextraction/reader/LogReader.java
@@ -1,33 +1,47 @@
 package at.anzola.gitlogextraction.reader;
 
 import at.anzola.gitlogextraction.response.Commit;
+import at.anzola.gitlogextraction.response.Log;
 import at.anzola.gitlogextraction.utlis.CommitDate;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 
+/**
+ * The LogReader class
+ *
+ * @author fabioanzola
+ */
 public class LogReader {
 
+    /**
+     * Regex for the commit hash
+     */
     private final static String REGEX_COMMIT = "^(commit\\s[a-f0-9]{40})";
 
+    /**
+     * Regex for the author line
+     */
     private final static String REGEX_AUTHOR = "^(Author:\\s.+)";
 
+    /**
+     * Regex for the Date line
+     */
     private final static String REGEX_DATE = "^(Date:[\\s]{3}[A-Za-z]{3}\\s[A-Za-z]{3}\\s[0-9]{1,2}\\s[0-9:]{8}\\s[0-9]{4}\\s[0-9+]{5})";
 
-    private final static String REGEX_FULL_COMMIT = ""
-            + "commit\\s[a-f0-9]{40}\\s+" +
-            "(Merge: [a-f0-9 ]+)?\\s+" +
-            "Author:\\s.+\\n" +
-            "Date:[\\s]{3}[A-Za-z]{3}\\s[A-Za-z]{3}\\s[0-9]{2}\\s[0-9:]{8}\\s[0-9]{4}\\s[0-9+]{5}" +
-            "\\s+" +
-            ".+";
-
-    public static void read(String file) throws IOException {
+    /**
+     * Reads the log and outputs the object
+     *
+     * @param file The log file
+     * @return The log object
+     * @throws IOException Thrown if an error occurs
+     */
+    public static Log read(String file) throws IOException {
+        Log log = new Log();
 
         try (
                 BufferedReader in = Files.newBufferedReader(Paths.get(file), StandardCharsets.UTF_8)
@@ -46,7 +60,7 @@ public class LogReader {
             while ((line = in.readLine()) != null) {
                 if (line.matches(REGEX_COMMIT) && !hash.equals("")) {
                     Commit commit = new Commit(hash, author, authorDate, message);
-                    System.out.println(commit);
+                    log.gitLog.add(commit);
                     hash = "";
                     author = "";
                     authorDate = null;
@@ -55,20 +69,16 @@ public class LogReader {
 
                 if (line.matches(REGEX_COMMIT)) {
                     hash = line.split(" ")[1].trim();
-                }
-                else if (line.matches(REGEX_AUTHOR)) {
+                } else if (line.matches(REGEX_AUTHOR)) {
                     author = line.substring(7).trim();
-                }
-                else if (line.matches(REGEX_DATE)) {
+                } else if (line.matches(REGEX_DATE)) {
                     authorDate = CommitDate.of(line.substring(7).trim());
-                }
-                else {
+                } else {
                     if (!line.equals("")) {
                         if (!message.equals("")) {
                             message += ':';
                             message += line.trim();
-                        }
-                        else {
+                        } else {
                             message += line.trim();
                         }
                     }
@@ -76,9 +86,8 @@ public class LogReader {
 
             }
             Commit commit = new Commit(hash, author, authorDate, message);
-            System.out.println(commit);
+            log.gitLog.add(commit);
         }
+        return log;
     }
-
-
 }
