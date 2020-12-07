b38d5d4398166b840fe4ec8f26b204f64ac78afd
Anzola Fabio <anzola.fabio@gmail.com>
2020-12-07 14:33:37 +0100

    poc for file open

--- src/at/anzola/gitlogextraction/ui/App.java
+++ src/at/anzola/gitlogextraction/ui/App.java
@@ -10,15 +10,18 @@ import java.io.IOException;
 
 public class App extends Application {
 
+    public static Stage stage;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+        stage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("applayout.fxml"));
-        primaryStage.setTitle("Git Log Extraction App");
-        primaryStage.setScene(new Scene(root, 800, 500));
-        primaryStage.show();
+        stage.setTitle("Git Log Extraction App");
+        stage.setScene(new Scene(root, 800, 500));
+        stage.show();
     }
 }
--- src/at/anzola/gitlogextraction/ui/AppController.java
+++ src/at/anzola/gitlogextraction/ui/AppController.java
@@ -1,6 +1,18 @@
 package at.anzola.gitlogextraction.ui;
 
 import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class AppController {
+
+    @FXML
+    public void open() {
+        final FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(App.stage);
+        if (file != null) {
+            System.out.println(file);
+        }
+    }
 }
