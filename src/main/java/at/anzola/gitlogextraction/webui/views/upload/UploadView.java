516a1b2b1a8b6b3b12ba6d99cf007333293092ef
Anzola Fabio <anzola.fabio@gmail.com>
2020-12-29 15:42:13 +0100

    Small refactoring for UploadView

--- src/main/java/at/anzola/gitlogextraction/webui/views/upload/UploadView.java
+++ src/main/java/at/anzola/gitlogextraction/webui/views/upload/UploadView.java
@@ -1,36 +1,21 @@
 package at.anzola.gitlogextraction.webui.views.upload;
 
 import at.anzola.gitlogextraction.reader.LogReader;
-import at.anzola.gitlogextraction.response.Log;
-import at.anzola.gitlogextraction.webui.views.list.ListView;
+import at.anzola.gitlogextraction.webui.views.main.MainView;
 import com.vaadin.flow.component.*;
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
-import com.vaadin.flow.internal.MessageDigestUtil;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import at.anzola.gitlogextraction.webui.views.main.MainView;
 import com.vaadin.flow.router.RouteAlias;
-import com.vaadin.flow.server.StreamResource;
-import org.apache.commons.io.IOUtils;
 
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.stream.ImageInputStream;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
 
 @Route(value = "upload", layout = MainView.class)
 @PageTitle("Upload")
@@ -65,67 +50,17 @@ public class UploadView extends HorizontalLayout {
                 e.printStackTrace();
             }
         });
+
         add(upload, output);
     }
 
-    private Component createComponent(String mimeType, String fileName,
-                                      InputStream stream) {
-        if (mimeType.startsWith("text")) {
-            return createTextComponent(stream);
-        } else if (mimeType.startsWith("image")) {
-            Image image = new Image();
-            try {
-
-                byte[] bytes = IOUtils.toByteArray(stream);
-                image.getElement().setAttribute("src", new StreamResource(
-                        fileName, () -> new ByteArrayInputStream(bytes)));
-                try (ImageInputStream in = ImageIO.createImageInputStream(
-                        new ByteArrayInputStream(bytes))) {
-                    final Iterator<ImageReader> readers = ImageIO
-                            .getImageReaders(in);
-                    if (readers.hasNext()) {
-                        ImageReader reader = readers.next();
-                        try {
-                            reader.setInput(in);
-                            image.setWidth(reader.getWidth(0) + "px");
-                            image.setHeight(reader.getHeight(0) + "px");
-                        } finally {
-                            reader.dispose();
-                        }
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return image;
-        }
-        Div content = new Div();
-        String text = String.format("Mime type: '%s'\nSHA-256 hash: '%s'",
-                mimeType, MessageDigestUtil.sha256(stream.toString()));
-        content.setText(text);
-        return content;
-
-    }
-
-    private Component createTextComponent(InputStream stream) {
-        String text;
-        try {
-            text = IOUtils.toString(stream, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            text = "exception reading stream";
-        }
-        return new Text(text);
-    }
-
-    private void showOutput(String text, Component content,
-                            HasComponents outputContainer) {
+    private void showOutput(String text, Component content, HasComponents outputContainer) {
         HtmlComponent p = new HtmlComponent(Tag.P);
         p.getElement().setText(text);
         outputContainer.add(p);
         outputContainer.add(content);
     }
 
-    //TODO Page for Log uploading
+    //TODO Design Page for Log uploading
 
 }
